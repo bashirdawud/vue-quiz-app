031157ab2b5154e550cd56ae1bea163e7eefebd9
bashirdawud <bashir.dauda@kadet.io>
2020-11-16 11:22:12 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+// eslint-disable-next-line vue/valid-template-root
+<template>
+    <b-nav tabs>
+        <b-nav-item disabled> <b>Fancy quiz app</b> </b-nav-item>
+        <b-nav-item disabled>Counter : {{numCorrect}}/{{numTotal}}</b-nav-item>
+        
+    </b-nav>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
